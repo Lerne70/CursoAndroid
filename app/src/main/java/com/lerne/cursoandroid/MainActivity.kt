f6c45cab521de92e28474eebb0d5f098989d028b
Lerne70 <56684878+Lerne70@users.noreply.github.com>
2022-01-24 19:56:35 -0600

    Ciclo de vida

--- app/src/main/java/com/lerne/cursoandroid/MainActivity.kt
+++ app/src/main/java/com/lerne/cursoandroid/MainActivity.kt
@@ -2,11 +2,60 @@ package com.lerne.cursoandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "Lifecycle"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        val texto = findViewById<TextView>(R.id.txt_output)
+        val boton = findViewById<Button>(R.id.btn_accion)
+        boton.setOnClickListener {
+            texto.text = "Curso Android"
+        }
+
+        Log.d(TAG, "onCreate")
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(TAG, "onDestroy")
+    }
+
 }
